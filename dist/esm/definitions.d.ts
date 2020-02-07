e2a50540edc431f3b53ee16d6b128793b146338b
FrancisD <francisdurante11@gmail.com>
2020-02-07 13:35:16 +0800

    test1

---
+++ dist/esm/definitions.d.ts
@@ -0,0 +1,12 @@
+declare module "@capacitor/core" {
+    interface PluginRegistry {
+        GalleryPlugin: GalleryPluginPlugin;
+    }
+}
+export interface GalleryPluginPlugin {
+    echo(options: {
+        value: string;
+    }): Promise<{
+        value: string;
+    }>;
+}
