d4094a838bdef2178af98de7df0b16f63ab032bc
FrancisD <francisdurante11@gmail.com>
2020-02-10 09:55:22 +0800

    android manifest

--- android/src/main/java/com/gallery/plugin/GalleryMainActivity.java
+++ android/src/main/java/com/gallery/plugin/GalleryMainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.gallery.plugin.galleryplugin.R;
 
@@ -11,6 +13,9 @@ public class GalleryMainActivity extends AppCompatActivity {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main_gallery);
     }
 }
