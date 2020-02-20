df046856f9b17242a9c81f68254288c0ea18dac8
FrancisD <francisdurante11@gmail.com>
2020-02-20 14:56:04 +0800

    re-layout

--- android/src/main/java/com/gallery/plugin/GalleryMainActivity.java
+++ android/src/main/java/com/gallery/plugin/GalleryMainActivity.java
@@ -1,30 +1,52 @@
 package com.gallery.plugin;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.IdRes;
 import android.support.annotation.NonNull;
+import android.support.design.bottomnavigation.LabelVisibilityMode;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.internal.BottomNavigationMenuView;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.gallery.plugin.galleryplugin.R;
 
 public class GalleryMainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
-  private static final int REQUEST_WRITE_PERMISSION = 411;
-
+    private static final int REQUEST_WRITE_PERMISSION = 411;
+    private BottomNavigationView bottomNavigationView;
+    private View notificationBadge;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_gallery);
-        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+        bottomNavigationView = findViewById(R.id.navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        bottomNavigationView.inflateMenu(R.menu.my_navigation_items);
+        bottomNavigationView.setLabelVisibilityMode(LabelVisibilityMode.LABEL_VISIBILITY_LABELED);
+        addBadgeView();
         requestPermission();
     }
+    private void addBadgeView() {
+        BottomNavigationMenuView menuView = (BottomNavigationMenuView) bottomNavigationView.getChildAt(0);
+        BottomNavigationItemView itemView = (BottomNavigationItemView) menuView.getChildAt(2);
+
+        notificationBadge = LayoutInflater.from(this).inflate(R.layout.notification_badge, menuView, false);
+
+        itemView.addView(notificationBadge);
+    }
+
   @Override
   public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
     if (requestCode == REQUEST_WRITE_PERMISSION && grantResults[0] == PackageManager.PERMISSION_GRANTED
@@ -59,11 +81,20 @@ public class GalleryMainActivity extends AppCompatActivity implements ActivityCo
       switch (menuItem.getTitle().toString()) {
 
         case "Pictures":
-          fragment = new PictureFragment();
-          break;
+              fragment = new PictureFragment();
+              break;
         case "Videos":
-          fragment = new VideosFragment();
-          break;
+              fragment = new VideosFragment();
+              break;
+
+        case "Live":
+              GalleryPlugin.returnResponse("request_live");
+              finish();
+              break;
+        case "Pending":
+             GalleryPlugin.returnResponse("request_pending");
+             finish();
+             break;
       }
 
       return loadFragment(fragment);
